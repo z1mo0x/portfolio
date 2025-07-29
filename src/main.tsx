6fd8cc7ce604d6b88f9a00d2492f4468e2b8d655
z1mo0x <droweckiy@mail.ru>
2025-07-29 07:20:54 +0300

    first

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+])
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+)
